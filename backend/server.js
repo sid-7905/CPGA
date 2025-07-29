51222e70b96bc87015a8a7a80fa6ec1949f2e11f
Siddhesh Pandey <siddheshpandey7905@gmail.com>
2025-07-29 20:48:54 +0530

    change server file

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: [
+     origin: [
       "http://localhost:3000",
       "https://cpga-siddhesh-pandeys-projects.vercel.app",
       "https://cpga.vercel.app",
